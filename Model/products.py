a66d70fa44292067b0df4b4d5e2eaae55fd80f23
aliilan <aliilan1425@gmail.com>
2025-04-24 09:46:13 +0300

    Refactor ProductsManager to load product due times from a separate JSON file and update Product class to include due_time attribute

--- Model/products.py
+++ Model/products.py
@@ -7,26 +7,30 @@ class Product:
     id: int
     tests: List[int]
     voltage_requirements: List[int]
+    due_time: int = 0
 
     def __str__(self) -> str:
         return f"Product {self.id} (Tests {self.tests})"
     
 class ProductsManager:
-    def __init__(self, json_file: str = "products.json"):
+    def __init__(self) -> None:
         self.products: List[Product] = []
 
-    def load_from_json(self, json_file: str) -> None:
+    def load_from_json(self, json_file_products: str, json_file_due_times) -> None:
         try:
-            df = pd.read_json(json_file)
+            df = pd.read_json(json_file_products)
+            df_due_time = pd.read_json(json_file_due_times)
 
             product_data: int = 0
             row = df.iloc[product_data]
+            row_due_time = df_due_time.iloc[product_data]
             index = 0
-            for product, voltage in zip(row['product_matrix'], row['voltage_requirements']):
+            for product, voltage, due_time in zip(row['product_matrix'], row['voltage_requirements'], row_due_time["product_due_time"]):
                 product = Product(
                     id=index,
                     tests=product,
-                    voltage_requirements=voltage
+                    voltage_requirements=voltage,
+                    due_time=due_time
                 )
                 self.products.append(product)
                 index += 1
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import random
 from Algorithm.scheduler_ver2 import SchedulerVer2
 from Model.chambers import ChamberManager
-from Model.product_tests import ProductTest, TestManager
+from Model.product_tests import TestManager
 from Model.products import ProductsManager
-#from Model.task_times import Task
-from Model.task import Task
 from gantt_chart import gantt_chart
 from gantt_chart_product import gantt_chart_product
-from Algorithm.scheduler import Scheduler
+
 
 def main():
-
 
     chamber_data_path: str = "Data/chambers.json"
     test_data_path: str = "Data/tests.json"
     product_data_path: str = "Data/products.json"
+    product_due_time_path: str = "Data/products_due_time.json"
 
     chamber_manager : ChamberManager = ChamberManager()
     chamber_manager.load_from_json(chamber_data_path)
@@ -23,7 +20,7 @@ def main():
     test_manager.load_from_json(test_data_path)
 
     product_manager : ProductsManager = ProductsManager()
-    product_manager.load_from_json(product_data_path)
+    product_manager.load_from_json(product_data_path, product_due_time_path)
 
     # scheduler = Scheduler(chamber_manager.chambers, test_manager.tests)
     # scheduler.first_come_first_served(product_manager.products)
